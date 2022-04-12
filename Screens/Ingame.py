7e676475c2c6853efdd77f9cdb73b1fcdc24f6e9
Daan Breur <git@daanbreur.systems>
2022-04-12 22:10:17 +0200

    almost completely fixed ingame

--- Screens/Ingame.py
+++ Screens/Ingame.py
@@ -269,8 +269,8 @@ class Ingame:
             if event.key == pygame.K_f:
                 self.f_key_pressed = False
 
-    def update(self, dt: float) -> None:
-        self.game.group.update(dt)
+    def update(self, dt_: float) -> None:
+        self.game.group.update(dt_)
 
         def add_and_hide_skill(layername: str, skill: Skills) -> None:
             set_layer_visibility(self.game.tmx_data, self.game.map_layer, layername, False)
@@ -284,12 +284,12 @@ class Ingame:
             set_layer_visibility(self.game.tmx_data, self.game.map_layer, layername, False)
 
         for sprite in self.game.group.sprites():
-            self.game.doorManager.update(sprite, dt)
+            self.game.doorManager.update(sprite, dt_)
 
             if sprite.feet.collidelist(self.walls) > -1:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
             if sprite.feet.collidelist(self.shallowwater) > -1 and not Skills.SWIM in sprite.skills:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
 
             if sprite.feet.colliderect(self.blue_gem_collider) and not check_gem(sprite, Gems.BLUE):
                 add_and_hide_gem("blue_gem", Gems.BLUE)
@@ -318,17 +318,17 @@ class Ingame:
             if sprite.feet.collidelist(self.soft_wall_low_range) > -1 and check_skill(sprite, Skills.HAMMER):
                 hide_layer("soft_wall_low")
             if sprite.feet.collidelist(self.soft_wall_low_collider) > -1 and not check_skill(sprite, Skills.HAMMER):
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
             if sprite.feet.collidelist(self.soft_wall_high_range) > -1 and check_skill(sprite, Skills.HAMMER):
                 hide_layer("soft_wall_high")
             if sprite.feet.collidelist(self.soft_wall_high_collider) > -1 and not check_skill(sprite, Skills.HAMMER):
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
 
             if sprite.feet.colliderect(self.gear_wall_range) and check_gem(sprite, Gems.GEAR):
                 self.gear_wall_opened = True
                 hide_layer("gear_wall")
             if sprite.feet.collidelist(self.gear_wall_collider) > -1 and not self.gear_wall_opened:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
 
             if sprite.feet.colliderect(self.gem_door_range) and check_gem(sprite, Gems.BLUE) and check_gem(sprite, Gems.RED) and check_gem(sprite, Gems.GREEN):
                 self.game.doorManager.open_door_by_id("gem_door_one")
@@ -338,16 +338,19 @@ class Ingame:
                 self.water_wall_opened = True
                 hide_layer("water_empty")
             if sprite.feet.collidelist(self.shallow_water_finish) > -1 and not self.water_wall_opened:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
 
             if sprite.feet.colliderect(self.music_door_collider) and not self.music_door_opened:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
 
-            if sprite.feet.colliderect(self.konami_sign): self.konami_text_show = True
+            if sprite.feet.colliderect(self.konami_sign):
+                self.konami_text_show = True
             else: self.konami_text_show = False
-            if sprite.feet.colliderect(self.simon_sign_range): self.simon_text_show = True
+            if sprite.feet.colliderect(self.simon_sign_range):
+                self.simon_text_show = True
             else: self.simon_text_show = False
-            if sprite.feet.colliderect(self.beach_sign_range): self.math_text_show = True
+            if sprite.feet.colliderect(self.beach_sign_range):
+                self.math_text_show = True
             else: self.math_text_show = False
 
             if sprite.feet.colliderect(self.button_door_one_range) and self.f_key_pressed is True:
@@ -358,7 +361,7 @@ class Ingame:
 
 
             if sprite.feet.colliderect(self.house_door_collider) and not self.door_lever_water_enabled:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
             if sprite.feet.colliderect(self.lever_water_range) and self.f_key_pressed is True:
                 self.f_key_pressed = False
                 self.door_lever_water_enabled = not self.door_lever_water_enabled
@@ -377,7 +380,7 @@ class Ingame:
 
 
             if sprite.feet.colliderect(self.lever_door_one_collider) and not self.door_lever_one_enabled:
-                sprite.move_back(dt)
+                sprite.move_back(dt_)
             if sprite.feet.colliderect(self.lever_spawn_range) and self.f_key_pressed is True:
                 self.f_key_pressed = False
                 self.door_lever_one_enabled = not self.door_lever_one_enabled
