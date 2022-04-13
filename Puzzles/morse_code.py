9e12708004ecf59ab6eaffff0dc92079f71cf3e8
Daan Breur <git@daanbreur.systems>
2022-04-13 10:43:09 +0200

    file rename

--- Puzzles/morse_code.py
+++ Puzzles/morse_code.py
@@ -8,7 +8,7 @@ import pygame
 import logging
 from constants import RESOURCES_DIR
 
-from utils import resource_path, set_layer_visibilty, tile_object_to_rect
+from utils import resource_path, set_layer_visibility, tile_object_to_rect
 
 class MorseCode():
   def __init__(self, game: Game) -> None:
@@ -39,17 +39,17 @@ class MorseCode():
           if not self.solved:
             logging.info("Morse Code Solved")
             self.solved = True
-            self.game.toastManager.addToast("Morse Code Solved", 17)
-            self.game.doorManager.openDoorById("math_door")
-            self.game.doorManager.openDoorById("final_gem_door")
+            self.game.toastManager.add_toast("Morse Code Solved", 17)
+            self.game.doorManager.open_door_by_id("math_door")
+            self.game.doorManager.open_door_by_id("final_gem_door")
         else:
           self.reset()
 
   def reset(self) -> None:
     logging.info("Resetting Morse Code")
-    self.game.doorManager.closeDoorById("final_gem_door")
-    self.game.doorManager.closeDoorById("math_door")
-    self.game.toastManager.addToast("Morse Code Reset", 17)
+    self.game.doorManager.close_door_by_id("final_gem_door")
+    self.game.doorManager.close_door_by_id("math_door")
+    self.game.toastManager.add_toast("Morse Code Reset", 17)
     self.solved = False
     self.sequence = [4,3,6,5,1,2]
     self.enteredSequence = []
@@ -68,7 +68,7 @@ class MorseCode():
         if sprite.feet.colliderect(self.mors_button_red_range): self.reset()
         if sprite.feet.colliderect(self.mors_button_blue_range):
           logging.info("Playing sound for Morse Code")
-          self.game.toastManager.addToast("Playing audio for Morse Code Challange", 15)
+          self.game.toastManager.add_toast("Playing audio for Morse Code Challange", 15)
           pygame.mixer.music.load(resource_path( RESOURCES_DIR / "sounds/morselevel.wav" ))
           pygame.mixer.music.set_volume(0.7)
           pygame.mixer.music.play()
